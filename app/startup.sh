d65370387de82c6d0cb059eb3c29e3a742e9e3a3
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2021-09-23 12:15:57 +0900

    MOD pycacheの生成を抑制
    - 変更がリアルタイムに反映されないので

--- app/startup.sh
+++ app/startup.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-python3 /usr/src/app/feed_generator.py
-python3 /usr/src/app/scheduler.py &
+python3 -B /usr/src/app/feed_generator.py
+python3 -B /usr/src/app/scheduler.py &
 httpd -D FOREGROUND
