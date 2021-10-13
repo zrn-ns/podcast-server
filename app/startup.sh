1e514c492293d2052529a9cf99ade9f33a793982
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2021-10-13 20:12:37 +0900

    FIX シンボリックリンクの作成に失敗する問題の修正

--- app/startup.sh
+++ app/startup.sh
@@ -2,7 +2,8 @@
 
 set -eu
 
-ln -s /volumes/music_files /usr/local/apache2/htdocs/music_files
+cd /usr/local/apache2/htdocs/
+ln -s /volumes/music_files music_files
 python3 -B /usr/src/app/feed_generator.py
 python3 -B /usr/src/app/scheduler.py &
 httpd -D FOREGROUND
